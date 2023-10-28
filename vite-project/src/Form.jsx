83b09e6e5ce39be9fbd5452b88d7d79e804d2eca
Thomas <thomas.bogle@outlook.com>
2023-10-28 22:46:19 +0100

    hr and svg added to bottom of Form component

--- vite-project/src/Form.jsx
+++ vite-project/src/Form.jsx
@@ -1,3 +1,5 @@
+import "./form.css";
+
 function Form() {
   return (
     <>
@@ -17,11 +19,16 @@ function Form() {
           <input type="datetime" name="year-input" placeholder="YYYY" maxLength="4" />
         </div>
       </form>
+
+      <div className="submit-wrapper">
+        <hr className="horizonal-line"/>
+        <svg className="submit-arrow" xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" strokeWidth="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
+      </div>
     
-      <div className="attribution">
+      {/* <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
-      </div>
+      </div> */}
     </>
   );
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import Form from './Form.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
