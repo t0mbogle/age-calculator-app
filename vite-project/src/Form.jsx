d716c35f4dab105dfa13ebd524ef2a480519b249
Thomas <thomas.bogle@outlook.com>
2024-01-09 21:38:38 +0000

    Restyled form to match mobile design

--- vite-project/src/Form.jsx
+++ vite-project/src/Form.jsx
@@ -3,19 +3,19 @@ import "./form.css";
 function Form() {
   return (
     <>
-      <form className="form">
+      <form>
         <div className="input-div">
-          <label htmlFor="day-input">DAY</label><br />
+          <label htmlFor="day-input">DAY</label>
           <input type="datetime" name="day-input" placeholder="DD" maxLength="2" />
         </div>
   
         <div className="input-div">
-          <label htmlFor="month-input">MONTH</label><br />
+          <label htmlFor="month-input">MONTH</label>
           <input type="datetime" name="month-input" placeholder="MM" maxLength="2" />
         </div>
   
         <div className="input-div">
-          <label htmlFor="year-input">YEAR</label><br />
+          <label htmlFor="year-input">YEAR</label>
           <input type="datetime" name="year-input" placeholder="YYYY" maxLength="4" />
         </div>
       </form>
