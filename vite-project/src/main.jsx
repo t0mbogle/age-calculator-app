c81ec02947dbdb9eccc8b63d60df962150c09b6c
Thomas <thomas.bogle@outlook.com>
2024-01-14 13:02:10 +0000

    Styling for root element

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Form from './Form.jsx'
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
