045443293c2cc3b71f5aff5480c1945ab35a2a50
Thomas <thomas.bogle@outlook.com>
2024-01-17 18:22:27 +0000

    Added validation check for future dates

--- vite-project/src/utils/validateDob.js
+++ vite-project/src/utils/validateDob.js
@@ -6,11 +6,6 @@ const validateDob = (dob) => {
     const DOB_INPUT_KEYS = ['days', 'months', 'years'];
     let errors = {days: '', months: '', years: ''};
 
-
-    // const ageInMs = Math.abs(new Date() - new Date(`${dob['years']}/${dob['months']}/${dob['days']}`));
-    // console.log(ageInMs);
-
-
     // EMPTY ENTRIES
     for (const key of DOB_INPUT_KEYS) {
         if (!Object.prototype.hasOwnProperty.call(dob, key)) {
@@ -18,19 +13,29 @@ const validateDob = (dob) => {
         }
     }
     
-    // // VALID TYPES & INPUTS FOR DD/MM/YYYY
+    // VALID TYPES & INPUTS FOR DD/MM/YYYY
     if ((!errors.days) && (dob.days > 31 || isNaN(dob.days))) {
         errors.days = 'Must be a valid day';
     }
     if ((!errors.months) && ((dob.months - 1) > 11 || isNaN(dob.months))) {
         errors.months = 'Must be a valid month';
     }
-    if (dob.years > new Date().getFullYear()) {
-        errors.years = 'Must be in the past';
-    } else if ((!errors.years) && isNaN(dob.months)) {
+
+    // CHECK FOR FUTURE DATE
+    const now = new Date();
+    now.setHours(0, 0, 0, 0);
+    // set now to have the same HH/MM/SS as the entered dob
+    const ageInMs = (now - new Date(`${dob.years}/${dob.months}/${dob.days}`));
+    // console.log(ageInMs);
+    // console.log(typeof ageInMs);
+    // console.log(ageInMs <);
+
+    if ((!errors.years) && isNaN(dob.years)) {
         errors.years = 'Must be a valid year';
+    } else if ((!errors.years) && ageInMs < 0) {
+        errors.years = 'Must be in the past';
     }
-    
+
     // VALID DATE IN THE PAST
     const dateObject = new Date(dob.years, (dob.months - 1), dob.days);
     if ((!errors.days) && (dateObject.getFullYear() != dob.years || dateObject.getMonth() !== (dob.months - 1) || dateObject.getDate() != dob.days)) {
