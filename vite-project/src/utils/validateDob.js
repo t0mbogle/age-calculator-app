ffc40f8226eae1f2c99b22f52019ad0c9afeaa81
Thomas <thomas.bogle@outlook.com>
2024-01-17 17:39:37 +0000

    Converted date types into numbers

--- vite-project/src/utils/validateDob.js
+++ vite-project/src/utils/validateDob.js
@@ -1,7 +1,15 @@
 const validateDob = (dob) => {
-    const DOB_INPUT_KEYS = ['days', 'months', 'years'];
+    dob.days = Number(dob.days);
+    dob.months = Number(dob.months);
+    dob.years = Number(dob.years);
 
+    const DOB_INPUT_KEYS = ['days', 'months', 'years'];
     let errors = {days: '', months: '', years: ''};
+
+
+    // const ageInMs = Math.abs(new Date() - new Date(`${dob['years']}/${dob['months']}/${dob['days']}`));
+    // console.log(ageInMs);
+
 
     // EMPTY ENTRIES
     for (const key of DOB_INPUT_KEYS) {
@@ -10,28 +18,27 @@ const validateDob = (dob) => {
         }
     }
     
-    // VALID DATE IN THE PAST
-    const dateObject = new Date(dob.years, (dob.months - 1), dob.days);
-    if (dateObject.getFullYear() != dob.years || dateObject.getMonth() !== (dob.months - 1) || dateObject.getDate() != dob.days) {
-        errors.days = 'Must be a valid date';
-        //console.log('Must be a valid date');
-    }
-
-    // VALID TYPES & INPUTS FOR DD/MM/YYYY
-    if (errors.days === '' && (dob.days > 31 || typeof dob.days !== 'number')) {
+    // // VALID TYPES & INPUTS FOR DD/MM/YYYY
+    if ((!errors.days) && (dob.days > 31 || isNaN(dob.days))) {
         errors.days = 'Must be a valid day';
     }
-    if (errors.months === '' && ((dob.months - 1) > 11 || typeof dob.months !== 'number')) {
+    if ((!errors.months) && ((dob.months - 1) > 11 || isNaN(dob.months))) {
         errors.months = 'Must be a valid month';
     }
     if (dob.years > new Date().getFullYear()) {
         errors.years = 'Must be in the past';
-    } else if (errors.years === '' && dob.years !== 'number') {
+    } else if ((!errors.years) && isNaN(dob.months)) {
         errors.years = 'Must be a valid year';
     }
-
+    
+    // VALID DATE IN THE PAST
+    const dateObject = new Date(dob.years, (dob.months - 1), dob.days);
+    if ((!errors.days) && (dateObject.getFullYear() != dob.years || dateObject.getMonth() !== (dob.months - 1) || dateObject.getDate() != dob.days)) {
+        errors.days = 'Must be a valid date';
+        errors.months = '';
+        errors.years = '';
+    }
     console.log(errors);
 }
-
 
 export default validateDob;
