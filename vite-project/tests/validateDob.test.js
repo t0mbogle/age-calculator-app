60c57e243e74c8fd6b772af27a2184c4f33358b5
Thomas <thomas.bogle@outlook.com>
2024-01-24 00:34:01 +0000

    Test written for empty fields

--- vite-project/tests/validateDob.test.js
+++ vite-project/tests/validateDob.test.js
@@ -7,9 +7,18 @@ describe('validation on dob input', () => {
     test('invalid date', () => {
         const dob = { days: 31, months: 4, years: 1991 }
         const dobResponse = validateDob(now, dob);
-
-        let errors = {days: 'Must be a valid date', months: '', years: ''};
+        const errors = {days: 'Must be a valid date', months: '', years: ''};
 
         expect(errors).toEqual(dobResponse);
-    });     
+    });
+
+    test('all fields empty', () => {
+        const dob = { days: '', months: '', years: '' }
+        const dobResponse = validateDob(now, dob);
+        const errors = {days: 'This field is required', months: 'This field is required', years: 'This field is required'};
+
+        expect(errors).toEqual(dobResponse);
+    })
+    xtest('invalid entries', () => {})
+    xtest('', () => {})
 })
