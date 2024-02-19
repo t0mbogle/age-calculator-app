e843111c9bc477e24c5c17ff3cfac8243e989fac
Thomas <thomas.bogle@outlook.com>
2024-02-19 23:09:33 +0000

    added deploy scripts

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'age-calculator-app-vite-project',
   plugins: [react()],
+  base: "/age-calculator-app-main/",
 })
